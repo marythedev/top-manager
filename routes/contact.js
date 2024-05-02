9949226dfe9e663c5d481e71956018a90c2826dc
Maria <79389256+marythedev@users.noreply.github.com>
2024-05-02 19:49:06 -0400

    contact feature implemented with formspree

--- routes/contact.js
+++ routes/contact.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const contact_title = "Contact";  //html title for rendered file
+const contact_title = "Contact";  //page title for rendered file
 
+// render contact page
 router.get("/", (request, response) => {
     response.render("contact", { title: contact_title });
-});
-
-router.post('/', (request, response) => {
-    console.log("sending email...");
-    response.render("contact", { 
-        error: "Backend for contact form is not implemented yet! Try again later.",
-        title: contact_title 
-    });
 });
 
 module.exports = router;
