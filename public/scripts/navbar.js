92cda81c2ae3930f0bde8a68615a8d55cf2c7004
marythedev <marythedev@gmail.com>
2025-10-07 16:31:05 -0400

    implemented responsive navbar

---
+++ public/scripts/navbar.js
@@ -0,0 +1,79 @@
+
+
+// Desktop navbar dropdown
+
+const dropdown = document.getElementsByClassName("dropdown")[0];
+const dropdownContent = document.querySelector(".dropdown .card");
+const account = document.getElementById("account");
+const contact = document.getElementById("contact");
+const logout = document.getElementById("logout");
+
+dropdown.addEventListener("focus", () => {
+    dropdownContent.classList.remove("no-display");
+    dropdownContent.classList.add("block-display");
+});
+dropdown.addEventListener("blur", () => {
+    dropdownContent.classList.remove("block-display");
+    dropdownContent.classList.add("no-display");
+});
+
+account.addEventListener("click", () => {
+    window.location.href = "/account";
+});
+contact.addEventListener("click", () => {
+    window.location.href = "/contact";
+});
+logout.addEventListener("click", () => {
+    window.location.href = "/logout";
+});
+
+
+
+
+// Mobile navbar menu panel
+
+const mobileMenu = document.getElementById('mobile-menu');
+
+function openMenu() {
+    mobileMenu.style.display = 'flex';
+    mobileMenu.style.animation = "mobile-menu-open 500ms forwards ease";
+}
+
+function closeMenu() {
+    mobileMenu.style.animation = "mobile-menu-close 500ms forwards ease";
+
+    setTimeout(() => {
+        mobileMenu.style.display = 'none';
+    }, 500);
+}
+
+try {
+    const desktopMenuToggler = document.getElementById('desktop-menu-toggler');
+    let menuState = false; //closed
+
+    desktopMenuToggler.addEventListener('click', (e) => {
+        menuState ? closeMenu() : openMenu();
+        menuState = !menuState;
+    });
+} catch { /* desktopMenuToggler do not exist on mobile view DOM */ }
+
+
+try {
+    const mobileOpenMenu = document.getElementById('mobile-menu-icon');
+    const mobileCloseMenu = document.getElementById('close-menu-icon');
+
+    mobileOpenMenu.addEventListener('click', (e) => {
+
+        // toggle menu icons form display
+        mobileOpenMenu.style.display = 'none';
+        mobileCloseMenu.style.display = 'block';
+
+        openMenu();
+    });
+
+    mobileCloseMenu.addEventListener('click', (e) => {
+        mobileCloseMenu.style.display = 'none';
+        mobileOpenMenu.style.display = 'block';
+        closeMenu();
+    });
+} catch {/* menuIcons do not exist on desktop view DOM */} 
